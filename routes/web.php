62c6c6f956615dec99d158fbafa46b9605416c10
Nico Deblauwe <nico@bluepundit.eu>
2026-09-03 21:49:26 +0200

    REFACTOR :hammer: Clean out the routes file

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
 use App\Http\Controllers\Userzone\DashboardController;
+use App\Http\Controllers\Userzone\ProfileController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
+/*
+ * Public Website routes
+ */
 Route::get('/', WelcomeController::class)->name('welcome');
 
-Route::get('/dashboard', DashboardController::class)
-    ->middleware(['auth', 'verified'])->name('dashboard');
+// Todo: add your public routes here
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
+/*
+ * Authentication routes
+ */
 require __DIR__.'/auth.php';
+
+
+/*
+ * Userzone routes
+ */
+Route::middleware('auth')->group(function () {
+    // For the user's dashboard (after login)
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+    // Todo: add your Userzone routes here
+
+    // For the user's profile management
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
